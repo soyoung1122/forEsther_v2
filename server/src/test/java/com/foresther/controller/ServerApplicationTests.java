13e27552dfe0029f8e8c09e7d9636bdecc4d4d44
soyoung leee <134457816+soyoung1122@users.noreply.github.com>
2023-08-25 16:29:02 +0900

    [FIX] table accordion : mv accordion event tr to Button component due to routing error

---
+++ server/src/test/java/com/foresther/controller/ServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.foresther.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("dddd");
+	}
+
+}
