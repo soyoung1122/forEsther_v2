e26272fefa85bc278b9e948020f3ad11cbdba51d
esther <wjswls109@naver.com>
2023-09-05 10:30:26 +0900

    [ADD] 품목 검색 기능 구현

--- server/src/main/java/com/foresther/www/item/ItemMapper.java
+++ server/src/main/java/com/foresther/www/item/ItemMapper.java
@@ -3,8 +3,11 @@ package com.foresther.www.item;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface ItemMapper {
     public List<ItemVO> selectItems();
+
+    public List<ItemVO> selectItemBySearch(Map<String, Object> searchCriteria);
 }
--- server/src/main/java/com/foresther/www/item/ItemService.java
+++ server/src/main/java/com/foresther/www/item/ItemService.java
@@ -3,7 +3,10 @@ package com.foresther.www.item;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ItemService {
     public List<ItemVO> findItems();
+
+    public List<ItemVO> findItemBySearch(Map<String, Object> searchCriteria);
 }
