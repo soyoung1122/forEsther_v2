625b613b90bc08f5c22e70156088d93963e9cf2f
esther <wjswls109@naver.com>
2023-09-07 21:59:30 +0900

    [FIX] 엑셀 파일 양식 변경

--- client/src/utils/downloadExcel.js
+++ client/src/utils/downloadExcel.js
@@ -6,19 +6,17 @@ const downloadExcel = async () => {
   const workbook = new ExcelJS.Workbook();
     //sheet 생성
     const worksheet = workbook.addWorksheet("item-list"); 
-
-    //TITLE
-    worksheet.mergeCells('A1', 'G2');
-    worksheet.getCell('A1').value = '** 품목 등록 파일 **';
-    worksheet.getCell('A1').alignment = { vertical: 'middle', horizontal: 'center'};
-
-
-    worksheet.mergeCells('A3', 'G3');
-    worksheet.getCell('A3').value = '[품목 데이터를 각 컬럼에 맞추어 입력해주세요]';
-    worksheet.getCell('A3').alignment = { vertical: 'middle', horizontal: 'center'};
-
-    //Column Header
-    worksheet.getRow(5).values = ['품목명', '품목구분', '소분류', '규격', '안전재고량', '조달방법', '구매처명'];
+ 
+    //sheet 데이터 설정
+    worksheet.columns = [
+      { header: "품목명", width: 15 },
+      { header: "품목구분", width: 15 },
+      { header: "소분류", width: 15 },
+      { header: "규격", width: 15 },
+      { header: "안전재고량", width: 15 },
+      { header: "조달방법", width: 15 },
+      { header: "구매처명", width: 15 },
+    ];
 
     //다운로드
     const mimeType = { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" };
